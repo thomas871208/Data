120a947d02bc29096219d37bb46df28b62918b5a
Thomas <Thomas@Thomas_pc>
2019-09-11 22:14:45 +0800

    finish

---
+++ Data/src/tts/com/Homework1.java
@@ -0,0 +1,31 @@
+package tts.com;
+
+
+
+public class Homework1 {
+
+	public static void main(String[] args) {
+		int i;
+		int j;
+		int min;
+		int temp;
+		int[] data = {240,1,520,29,106};
+
+
+		for (i=0; i<5; i++){
+			min = i;
+			for (j=i+1; j<5; j++) 
+				if (data[j]<data[min]) 
+					min = j;temp = data[i];
+					data[i] = data[min];
+					data[min] = temp;
+					}
+			
+			
+		for(int a = 0; a<5;a++) {
+		System.out.print(data[a]+" ");
+		}
+
+	}
+
+}
